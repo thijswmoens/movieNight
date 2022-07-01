986044eb26fcc235973f628994fa6c123ed6f0e4
Thijs Moens <post@thijs.email>
2022-07-01 12:52:33 +0200

    📦 NEW: Connect to MongoDB

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/', (req, res ) => {
-    res.send('Movie Night!')
+    res.render('index')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+require('dotenv').config();
+
+//Check if we are in production envirment
+// if(process.env.NODE_ENV !== 'production') {
+//     require('dotenv').parse()
+// }
+
 // Import
 const express = require("express")
 const app = express()
@@ -13,6 +20,15 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use('/', indexRouter)
+
+// Setup MongoDB database
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose'))
 
 // Listen
 app.listen(process.env.PORT || 3000)
