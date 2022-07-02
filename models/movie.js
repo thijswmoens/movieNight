015a618f2ca8fb217bdff9a7407178e95a2247da
Thijs Moens <post@thijs.email>
2022-07-02 09:48:43 +0200

    👌 IMPROVE: Create more movie fields

--- models/movie.js
+++ models/movie.js
@@ -1,10 +1,37 @@
 const mongoose = require('mongoose')
 
+const coverImageBasePath = 'uploads/movieCovers'
+
 const movieSchema = new mongoose.Schema({
-    name: {
+    title: {
         type: String,
         required: true
+    },
+    description: {
+        type: String,
+    },
+    releaseDate: {
+        type: Date,
+        required: true
+    },
+    duration: {
+        type: Number,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    coverImageName: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Genre'
     }
 })
 
 module.exports = mongoose.model('Movie', movieSchema)
+module.exports.coverImageBasePath = coverImageBasePath
--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,18 @@
 // Import
 const express = require("express")
 const router = express.Router()
+const multer = require('multer')
+const path = require('path')
 const Movie = require('../models/movie')
+const Genre = require('../models/genre')
+const uploadPath = path.join('public', Movie.coverImageBasePath)
+const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gif']
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
+    }
+})
 
 // All Movies Route
 router.get('/', async (req, res ) => {
@@ -21,27 +32,42 @@ router.get('/', async (req, res ) => {
 })
 
 // New Movie Route
-router.get('/new', (req, res) => {
-    res.render('movies/new', {
-        movie: new Movie()
-    })
+router.get('/new', async (req, res) => {
+    renderNewPage(res, new Movie())
 })
 
 // Create new Movie
-router.post('/', async (req, res) => {
+router.post('/', upload.single('cover'), async (req, res) => {
+    const fileName = req.file != null ? req.file.filename : null
     const movie = new Movie({
-        name: req.body.name
+        title: req.body.title,
+        genre: req.body.genre,
+        releaseDate: new Date(req.body.releaseDate),
+        duration: req.body.duration,
+        coverImageName: fileName,
+        description: req.body.description
     })
     try {
-        const newMovie = await movie.save()
+        const newMovie = movie.save()
         // res.redirect(`movies/${newMovie.id}`)
         res.redirect(`movies`)
     } catch {
-        res.render('movies/new', {
-            movie: movie,
-            errorMessage: 'Error creating Movie'
-        })
+        renderNewPage(res, movie, true)
     }
 })
+
+async function renderNewPage(res, movie, hasError = false) {
+    try {
+        const genres = await Genre.find({})
+        const params = {
+            genres: genres,
+            movie: movie
+        }
+        if(hasError) params.errorMessage = 'Error Creating Movie'
+        res.render('movies/new', params)
+    } catch {
+        res.redirect('/movies')
+    }
+}
 
 module.exports = router
